4fcef88fd7ce4655e2b43a02a4ac5ccf51b9cf5c
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2023-01-24 09:32:12 +0300

    image slider

---
+++ src/Components/HeroSlider.jsx
@@ -0,0 +1,8 @@
+import imageSlide from "./data";
+const HeroSlider = ({slides}) => {
+    return(
+        <div></div>
+    )
+}
+
+export default HeroSlider;
---
+++ src/Components/data.js
@@ -0,0 +1,13 @@
+const imageSlide = [
+    {
+        url: "1.jpg"
+    },
+    {
+        url: "2.jpg"
+    },
+    {
+        url: "3.jpg"
+    }
+]
+
+export default imageSlide
